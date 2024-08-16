bb29af4d244ecfe83062072a67da718f838179a1
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-08-16 13:24:05 +0300

    layout added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Register from "../pages/Register";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
@@ -10,31 +10,31 @@ import Users from "../pages/Users";
 import UserDetail from "../pages/UserDetail";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
 import PrivateRouter from "./PrivateRouter";
+import Layout from "../components/Layout";
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
       <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/blogs" element={<PrivateRouter />}>
-          <Route path="posts" element={<BlogPosts />} />
+        <Route element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="/blogs" element={<PrivateRouter />}>
+            <Route path="posts" element={<BlogPosts />} />
 
-          <Route path="posts/:postId" element={<BlogDetail />} />
+            <Route path="posts/:postId" element={<BlogDetail />} />
+          </Route>
+          <Route path="/users" element={<PrivateRouter />}>
+            <Route path="" element={<Users />} />
+            <Route path=":userId" element={<UserDetail />} />
+          </Route>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
-        <Route path="/users" element={<PrivateRouter />}>
-          <Route path="" element={<Users />} />
-          <Route path=":userId" element={<UserDetail />} />
-        </Route>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer/>
+
     </BrowserRouter>
   );
 };

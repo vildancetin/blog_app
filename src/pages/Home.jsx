10273cdd825a03a3480aec52f486ecd12748f2d5
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-09-04 21:44:20 +0300

    pickedpost component created and up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@
 import { Carousel } from "flowbite-react";
 import PostCard from "../components/PostCard";
 import EditorPickCard from "../components/EditorPickCard";
+import PickedPost from "./PickedPost";
 
 export default function Home() {
   return (
@@ -39,29 +40,7 @@ export default function Home() {
       </div>
       {/* Picked Post */}
       <div>
-        <div className="w-full h-62 relative px-10">
-          <div className="text-white absolute flex flex-col justify-center items-center inset-0">
-            <div className="px-4 py-2 rounded-2xl text-white bg-gray-400 bg-opacity-25 text-base font-semibold tracking-wider uppercase mb-6">
-              category
-            </div>
-            <div className="text-2xl font-bold mb-4">
-              Richird Norton photorealistic rendering as real photos
-            </div>
-            <div>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Molestiae tempora sit, repellat vero ipsa at?
-            </div>
-            <div className="border-t-2 w-11 border-white my-6"></div>
-            <div>26.08.2024</div>
-          </div>
-          <div>
-            <img
-              src="https://picsum.photos/900/300"
-              alt=""
-              className="w-full h-auto "
-            />
-          </div>
-        </div>
+        <PickedPost />
       </div>
       {/* Editör Pick Posts */}
       <div>
---
+++ src/pages/PickedPost.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const PickedPost = () => {
+  return (
+    <div className="w-full h-62 relative px-10">
+    <div className="text-white absolute flex flex-col justify-center items-center inset-0">
+      <div className="px-4 py-2 rounded-2xl text-white bg-gray-400 bg-opacity-25 text-base font-semibold tracking-wider uppercase mb-6">
+        category
+      </div>
+      <div className="text-2xl font-bold mb-4">
+        Richird Norton photorealistic rendering as real photos
+      </div>
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Molestiae tempora sit, repellat vero ipsa at?
+      </div>
+      <div className="border-t-2 w-11 border-white my-6"></div>
+      <div>26.08.2024</div>
+    </div>
+    <div>
+      <img
+        src="https://picsum.photos/900/300"
+        alt=""
+        className="w-full h-auto "
+      />
+    </div>
+  </div>
+  )
+}
+
+export default PickedPost
